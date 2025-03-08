6cedc170bc33678a7c8a70421b75505a9828492b
mehedihasan2810 <mehedi.hasan.webcraft@gmail.com>
2025-03-08 16:35:22 +0600

    chore: Update react-textarea-autosize and add UUID generation utility

---
+++ app/(tools-n-course)/(tools)/serenity/_components/chat-input.tsx
@@ -0,0 +1,97 @@
+import { Button } from "@/components/ui";
+import { cn } from "@/lib/utils";
+import { Attachment, ChatRequestOptions, CreateMessage, Message } from "ai";
+import { SendIcon } from "lucide-react";
+import { Dispatch, SetStateAction, useCallback, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { toast } from "sonner";
+
+export const ChatInput = ({
+  chatId,
+  input,
+  setInput,
+  isLoading,
+  stop,
+  attachments,
+  setAttachments,
+  messages,
+  setMessages,
+  append,
+  handleSubmit,
+  className,
+}: {
+  chatId: string;
+  input: string;
+  setInput: (value: string) => void;
+  isLoading: boolean;
+  stop: () => void;
+  attachments: Array<Attachment>;
+  setAttachments: Dispatch<SetStateAction<Array<Attachment>>>;
+  messages: Array<Message>;
+  setMessages: Dispatch<SetStateAction<Array<Message>>>;
+  append: (
+    message: Message | CreateMessage,
+    chatRequestOptions?: ChatRequestOptions
+  ) => Promise<string | null | undefined>;
+  handleSubmit: (
+    event?: {
+      preventDefault?: () => void;
+    },
+    chatRequestOptions?: ChatRequestOptions
+  ) => void;
+  className?: string;
+}) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const submitForm = useCallback(() => {
+    window.history.replaceState({}, "", `/serenity/${chatId}`);
+
+    handleSubmit(undefined, {
+      experimental_attachments: attachments,
+    });
+
+    setAttachments([]);
+  }, [
+    attachments,
+    handleSubmit,
+    setAttachments,
+    chatId,
+  ]);
+  return (
+    <div
+      className={cn(
+        " relative w-full rounded-[2rem] bg-secondary text-sm outline-none shadow flex items-center gap-1 pr-2 mb-2 overflow-hidden transition-all",
+        isFocused && "border border-ring outline-none ring-[4px] ring-ring/20",
+        className
+      )}
+    >
+      <TextareaAutosize
+        className="w-full px-4 py-4 outline-none resize-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(event) => {
+          if (
+            event.key === "Enter" &&
+            !event.shiftKey &&
+            !event.nativeEvent.isComposing
+          ) {
+            event.preventDefault();
+
+            if (isLoading) {
+              toast.error("Please wait for the model to finish its response!");
+            } else {
+              submitForm();
+            }
+          }
+        }}
+        autoFocus
+        maxRows={5}
+        placeholder="Type a message..."
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      <button className="rounded-full size-10 flex items-center justify-center bg-muted text-muted-foreground p-2">
+        <SendIcon />
+      </button>
+    </div>
+  );
+};
---
+++ app/(tools-n-course)/(tools)/serenity/_components/chat-panel.tsx
@@ -0,0 +1,294 @@
+"use client";
+
+import { Props as ChatInputProps } from "@/components/chat/input";
+import { ChatMessageList } from "@/components/chat/message-list";
+import { Message, useChat } from "ai/react";
+import { addMessage, createChat, getChat, getChatMessages } from "@/lib/db";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+// import { useSupabase } from "@/lib/supabase";
+import { Attachment } from "@/app/types";
+import { ArtifactMessagePartData, cn, convertFileToBase64 } from "@/lib/utils";
+// import { useRouter } from "next/navigation";
+import { useRouter } from "nextjs-toploader/app";
+import { useWhisper as useRealWhisper } from "@chengsokdara/use-whisper";
+import { Props as ReactArtifactProps } from "@/components/artifact/react";
+import { SyntheticEvent, useEffect, useState } from "react";
+import { useScrollAnchor } from "@/lib/hooks/use-scroll-anchor";
+import { toast } from "sonner";
+import { reportErrorAction } from "@/actions/report-error-via-mail";
+import { ChatInput } from "./chat-input";
+
+type Props = {
+  id: string | null;
+  userEmail: string;
+  userId: string;
+};
+
+export const ChatPanel = ({ id, userEmail, userId }: Props) => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  // State management
+  const [chatId, setChatId] = useState(id);
+  const [initialMessages, setInitialMessages] = useState<Message[]>([]);
+  const [fetchingMessages, setFetchingMessages] = useState(false);
+  const [currentArtifact, setCurrentArtifact] =
+    useState<ArtifactMessagePartData | null>(null);
+  const [attachments, setAttachments] = useState<Attachment[]>([]);
+  const [selectedArtifacts, setSelectedArtifacts] = useState<string[]>([]);
+  const [files, setFiles] = useState<FileList | null>(null);
+
+  const [activeChatTab, setActiveChatTab] = useState("studyBuddyGPT");
+  const [isChatCreating, setIsChatCreating] = useState(false);
+
+  const {
+    data: chat,
+    error,
+    isLoading: isChatLoading,
+  } = useQuery({
+    queryKey: ["chat", chatId],
+    queryFn: async () => await getChat({ chatId }),
+    enabled: !!chatId,
+  });
+
+  console.log({ chat });
+
+
+
+  // Fetch messages for existing chat
+  const fetchMessages = async () => {
+    if (chatId) {
+      setFetchingMessages(true);
+      const messages = await getChatMessages(chatId);
+      setInitialMessages(
+        messages.map((message) => ({
+          id: String(message.id),
+          role: message.role as Message["role"],
+          content: message.text,
+          experimental_attachments: (message.attachments as Attachment[]) || [],
+        }))
+      );
+      setFetchingMessages(false);
+    } else {
+      setInitialMessages([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  // Create new chat mutation
+  const createChatMutation = useMutation({
+    mutationFn: async ({
+      title,
+      type,
+      userId,
+    }: {
+      title: string;
+      firstMessage: Message;
+      secondMessage: Message;
+      type: string;
+      userId: string;
+    }) => await createChat({ title, userId, type }),
+    onSuccess: async (newChat, { firstMessage, secondMessage }) => {
+      // queryClient.setQueryData<Chat[]>(["chats"], (oldChats) => {
+      //   return [...(oldChats || []), newChat];
+      // });
+
+      setIsChatCreating(true);
+
+      setChatId(newChat.id);
+
+      console.log({ firstMessage, secondMessage });
+
+      await addMessage(
+        // supabase,
+        newChat.id,
+        firstMessage,
+        firstMessage.experimental_attachments
+      );
+      await addMessage(newChat.id, secondMessage);
+
+      await queryClient.invalidateQueries({ queryKey: ["chats"] });
+
+      setIsChatCreating(false);
+
+      router.push(`/chat/${newChat.id}`);
+    },
+  });
+
+  // Chat hook setup
+  const {
+    messages,
+    setMessages,
+    input,
+    setInput,
+    append,
+    stop: stopGenerating,
+    isLoading: generatingResponse,
+    handleSubmit,
+  } = useChat({
+    initialMessages,
+    onFinish: async (message) => {
+      console.log({ chatId, message });
+      if (chatId) {
+        await addMessage(chatId, message);
+      }
+    },
+    onError(error) {
+      console.log({ chatError: error.message });
+
+      toast.error(error.message, {
+        position: "top-center",
+      });
+      reportErrorAction({
+        userEmail,
+        errorMessage: error.message,
+        errorTrace: `[ChatPanel] [useChat] [onError] [app/components/chat/panel.tsx]`,
+        errorSourceUrl: "/chat",
+      });
+    },
+    sendExtraMessageFields: true,
+    // body: { user_email: session?.user.email || "userrrr" },
+  });
+
+  // Scroll as new messages are added
+  const { messagesRef, scrollRef, showScrollButton, handleManualScroll } =
+    useScrollAnchor(messages);
+
+  // Create new chat when conditions are met
+  useEffect(() => {
+    if (!chatId && messages.length === 2 && !generatingResponse) {
+      createChatMutation.mutate({
+        title: messages[0].content.slice(0, 100),
+        firstMessage: messages[0],
+        secondMessage: messages[1],
+        userId,
+        type: activeChatTab,
+      });
+    }
+  }, [chatId, messages, generatingResponse]);
+
+  // Whisper hook setup for voice input
+  const useWhispherHook = useRealWhisper;
+  // const useWhispherHook = "getSettings().openaiApiKey"
+  //   ? useRealWhisper
+  //   : useFakeWhisper;
+  const { recording, transcribing, transcript, startRecording, stopRecording } =
+    useWhispherHook({
+      apiKey: "getSettings().openaiApiKey",
+    });
+
+  // Update input with transcribed text
+  useEffect(() => {
+    if (!recording && !transcribing && transcript?.text) {
+      setInput((prev) => prev + ` ${transcript.text}`);
+    }
+  }, [recording, transcribing, transcript?.text, setInput]);
+
+
+
+  // Handle attachment management
+  const handleAddAttachment: ChatInputProps["onAddAttachment"] = (
+    newAttachments
+  ) => {
+    setAttachments((prev) => [...prev, ...newAttachments]);
+  };
+
+  const handleAddFiles = (newFiles: FileList) => {
+    setFiles(newFiles);
+    // setFiles((prev) => [...prev, ...newFiles]);
+  };
+
+  const handleRemoveAttachment: ChatInputProps["onRemoveAttachment"] = (
+    attachment
+  ) => {
+    setFiles(null);
+    setAttachments((prev) =>
+      prev.filter((item) => item.url !== attachment.url)
+    );
+  };
+
+  // Handle sending messages
+  const handleSend = async (event?: SyntheticEvent) => {
+    const query = input.trim();
+
+    if (!query) return;
+
+
+
+    const options = files ? { experimental_attachments: files } : {};
+    console.log({ files });
+    handleSubmit(event, {
+      body: {
+        userId,
+        user_email: userEmail,
+        activeChatTab: chat?.type ?? activeChatTab,
+      },
+      ...options,
+    });
+
+   
+
+    setInput("");
+    stopRecording();
+
+    const userAttachment = files
+      ? [
+          {
+            contentType: files[0].type,
+            name: files[0].name,
+            url: await convertFileToBase64(files[0]),
+          },
+        ]
+      : [];
+
+    if (chatId) {
+      await addMessage(
+        // supabase,
+        chatId,
+        { role: "user", content: query },
+        userAttachment
+      );
+    }
+
+    setAttachments([]);
+    setSelectedArtifacts([]);
+    setFiles(null);
+  };
+
+  console.log({ messages });
+
+  return (
+    <>
+      <div
+        className={cn(
+          "relative flex w-full pt-4 sm:pt-6 flex-1 overflow-x-hidden overflow-y-scroll"
+        )}
+        ref={scrollRef}
+      >
+        <div className="relative mx-auto flex h-full w-full max-w-3xl flex-1 flex-col md:px-2">
+          <ChatMessageList
+            messages={messages}
+            containerRef={messagesRef}
+            isLoading={generatingResponse}
+          />
+          <ChatInput
+            chatId={chatId ?? ""}
+            input={input}
+            setInput={setInput}
+            isLoading={generatingResponse}
+            stop={stopGenerating}
+            attachments={attachments}
+            setAttachments={setAttachments}
+            messages={messages}
+            setMessages={setMessages}
+            append={append}
+            handleSubmit={handleSubmit}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
