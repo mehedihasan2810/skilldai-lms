2e46bd82fc34d2ce5786fa0a377cbdb1accc494a
mehedihasan2810 <mehedi.hasan.webcraft@gmail.com>
2025-03-11 18:46:16 +0600

    refactor: enhance token reset functionality in Supabase route

--- app/api/reset-token/route.ts
+++ app/api/reset-token/route.ts
@@ -1,10 +1,10 @@
 import { createClient } from "@/lib/supabase/server";
 
 export const GET = async () => {
-  const supabase = await createClient()
+  const supabase = await createClient();
   const { data, error } = await supabase
     .from("token_usage")
-    .update({ total_tokens: 0 })
+    .update({ total_tokens: 0, input_token: 0, output_token: 0 })
     .neq("id", "4162c9fc-b3a1-47bf-bbb2-a8ccd3d903c7");
   // console.log({ data, error });
 
